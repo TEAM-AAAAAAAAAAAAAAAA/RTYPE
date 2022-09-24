3efe795eddef329cdf64c448acb7f245d3bbd648
Broke Machine <emile.ettel@epitech.eu>
2022-09-24 16:53:35 +0200

    feat: added sparseArray.hpp with sparse_array class

---
+++ src/ecs/sparseArray.hpp
@@ -0,0 +1,66 @@
+/*
+** EPITECH PROJECT, 2022
+** RTYPE
+** File description:
+** sparseArray
+*/
+
+#ifndef SPARSEARRAY_HPP_
+#define SPARSEARRAY_HPP_
+
+#include <iostream>
+#include <optional>
+
+template <typename Component> // You can also mirror the definition of std :: vector , that takes an additional allocator.
+class sparse_array
+{
+public:
+    using value_type = std::optional<Component>; // optional component type
+    using reference_type = value_type &;
+    using const_reference_type = value_type const &;
+    using container_t = std::vector<value_type>; // optionally add your allocator template here.
+    using size_type = typename container_t::size_type;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t ::const_iterator;
+
+public:
+    sparse_array() { _data.clear(); }                         // You can add more constructors.
+    sparse_array(sparse_array const &arr) { this = arr; }    // copy constructor
+    sparse_array(sparse_array &&arr) noexcept { this = arr } // move constructor
+    ~sparse_array();
+    sparse_array &operator=(sparse_array const &arr)
+    {
+    }     // copy assignment operator
+    sparse_array &operator=(sparse_array &&arr) noexcept
+    {
+    } // move assignment operator
+    reference_type operator[](size_t idx) { return _data.at(idx); }
+    const_reference_type operator[](size_t idx) const { return _data.at(idx); }
+    iterator begin() { return _data.begin(); }
+    const_iterator begin() const { return _data.begin(); }
+    const_iterator cbegin() const { return _data.cbegin(); }
+    iterator end() { return _data.end(); }
+    const_iterator end() const { return _data.end(); }
+    const_iterator cend() const { return _data.cend(); }
+    size_type size() const { return _data.size(); }
+    reference_type insert_at(size_type pos, Component const &comp)
+    {
+        return _data.insert(pos, comp);
+    }
+    reference_type insert_at(size_type pos, Component &&comp)
+    {
+        return _data.insert(pos, comp);
+    }
+    template <class... Params>
+    reference_type emplace_at(size_type pos, Params &&...); // optional
+    void erase(size_type pos)
+    {
+        _data.erase(pos);
+    }
+    size_type get_index(value_type const &) const;
+
+private:
+    container_t _data;
+};
+
+#endif /* !SPARSEARRAY_HPP_ */
