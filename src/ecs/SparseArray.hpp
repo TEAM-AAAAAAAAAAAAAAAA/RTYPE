8ef07c4e91bd0e222feb892ee28ec96d24b162ea
hourcadettej <julien.hourcadette@live.fr>
2022-10-01 20:28:20 +0200

    docs: add documentation into SparseArray.hpp

--- src/ecs/SparseArray.hpp
+++ src/ecs/SparseArray.hpp
@@ -13,120 +13,143 @@
 #include <exception>
 #include <iostream>
 
-template <typename Component>
-class SparseArray
-{
-public:
-    using value_type = std::optional<Component>;
-    using reference_type = value_type &;
-    using const_reference_type = value_type const &;
-    using container_t = std::vector<value_type>;
-    using size_type = typename container_t::size_type;
-    using iterator = typename container_t::iterator;
-    using const_iterator = typename container_t ::const_iterator;
+namespace ecs {
 
-public:
-    SparseArray() { _data.clear(); }
-    SparseArray(SparseArray const &other) : _data(other._data) {}
-    SparseArray(SparseArray &&other) noexcept : _data(std::move(other._data)) {}
-    ~SparseArray() = default;
-    SparseArray &operator=(SparseArray const &other)
-    {
-        for (auto &&i : other)
-        {
-            _data.insert(other);
-        }
-        return *this;
-    }
-    SparseArray &operator=(SparseArray &&other) noexcept
-    {
-        _data = std::move(other._data);
-        return *this;
-    }
-    reference_type operator[](size_t idx)
-    {
-        return _data[idx];
-    }
-    const_reference_type operator[](size_t idx) const
-    {
-        return _data[idx];
-    }
-    iterator begin()
-    {
-        return _data.begin();
-    }
-    const_iterator begin() const
-    {
-        return _data.begin();
-    }
-    const_iterator cbegin() const
-    {
-        return _data.cbegin();
-    }
-    iterator end()
-    {
-        return _data.end();
-    }
-    const_iterator end() const
-    {
-        return _data.end();
-    }
-    const_iterator cend() const
-    {
-        return _data.cend();
-    }
-    size_type size() const
-    {
-        return _data.size();
-    }
-    reference_type insert_at(size_type pos, Component const &c)
-    {
-        try
-        {
-            if (pos >= _data.capacity())
-                _data.resize(pos);
-            _data.emplace(_data.begin() + pos, c);
-        }
-        catch (std::exception &e)
-        {
-            std::cout << e.what() << std::endl;
-        }
-        return _data[pos];
-    }
-    reference_type insert_at(size_type pos, Component &&c)
-    {
-        try
-        {
-            if (pos >= _data.capacity())
-                _data.resize(pos + 1);
-            _data.emplace(_data.begin() + pos, std::move(c));
-        }
-        catch (std::exception &e)
-        {
-            std::cout << e.what() << std::endl;
-        }
-        return _data[pos];
-    }
-    template <class... Params>
-    reference_type emplace_at(size_type pos, Params &&...)
-    {
-    }
-    void erase(size_type pos)
-    {
-        _data[pos] = std::nullopt;
-    }
-    size_type get_index(value_type const &c) const
-    {
-        (void)c;
-        throw std::runtime_error("Not implemented");
-        // auto i = std::find(_data.begin(), _data.end(), c);
-        // if (i == _data.end())
-        //     throw std::runtime_error("Value not in SparseArray");
-        // return i - _data.begin();
-    }
+    /**
+     * SparseArray class is the component container of the ecs. \n
+     * This can be quite useful to store components that are defined for most entities,
+     * because you don't have to store the entity ID alongside the component.
+     * @tparam Component The type of component used as with template
+     */
+    template<typename Component>
+    class SparseArray {
+        public:
+            using value_type = std::optional<Component>;
+            using reference_type = value_type &;
+            using const_reference_type = value_type const &;
+            using container_t = std::vector<value_type>;
+            using size_type = typename container_t::size_type;
+            using iterator = typename container_t::iterator;
+            using const_iterator = typename container_t::const_iterator;
 
-private:
-    container_t _data;
-};
+        public:
+            /**
+             * Default constructor & destructor of the SparseArray class
+             */
+            SparseArray() { _data.clear(); }
+            SparseArray(SparseArray const &other) : _data(other._data) {}
+            SparseArray(SparseArray &&other) noexcept: _data(std::move(other._data)) {}
+            ~SparseArray() = default;
+
+            /**
+             * Operator copy of the SparseArray class
+             * @param other The SparseArray you want to copy
+             * @return The Sparse array present in the class at the moment
+             */
+            SparseArray &operator=(SparseArray const &other) {
+                for (auto &&i: other) {
+                    _data.insert(other);
+                }
+                return *this;
+            }
+
+            /**
+             * Operator move of the SparseArray class
+             * @param other The SparseArray you want to move
+             * @return The Sparse array present in the class at the moment
+             */
+            SparseArray &operator=(SparseArray &&other) noexcept {
+                _data = std::move(other._data);
+                return *this;
+            }
+
+            reference_type operator[](size_t idx) {
+                return _data[idx];
+            }
+
+            const_reference_type operator[](size_t idx) const {
+                return _data[idx];
+            }
+
+            iterator begin() {
+                return _data.begin();
+            }
+
+            const_iterator begin() const {
+                return _data.begin();
+            }
+
+            const_iterator cbegin() const {
+                return _data.cbegin();
+            }
+
+            iterator end() {
+                return _data.end();
+            }
+
+            const_iterator end() const {
+                return _data.end();
+            }
+
+            const_iterator cend() const {
+                return _data.cend();
+            }
+
+            size_type size() const {
+                return _data.size();
+            }
+
+            reference_type insert_at(size_type pos, Component const &c) {
+                try {
+                    if (pos >= _data.capacity())
+                        _data.resize(pos);
+                    _data.emplace(_data.begin() + pos, c);
+                }
+                catch (std::exception &e) {
+                    std::cout << e.what() << std::endl;
+                }
+                return _data[pos];
+            }
+
+            reference_type insert_at(size_type pos, Component &&c) {
+                try {
+                    if (pos >= _data.capacity())
+                        _data.resize(pos + 1);
+                    _data.emplace(_data.begin() + pos, std::move(c));
+                }
+                catch (std::exception &e) {
+                    std::cout << e.what() << std::endl;
+                }
+                return _data[pos];
+            }
+
+            template<class... Params>
+            reference_type emplace_at(size_type pos, Params &&...) {
+            }
+
+            void erase(size_type pos) {
+                _data[pos] = std::nullopt;
+            }
+
+            /**
+             * This function is used to get the index of the std::optional<Component> of the std::vector<std::optional<Component>> _data according to the given value_type (std::optional<Component>)
+             * @param c The std::optional<Component>, type of the component you want the index
+             * @return The size_type of the value_type's index you are looking for
+             */
+            size_type get_index(value_type const &c) const {
+                for (auto iterator =  _data.begin(); iterator != _data.end(); iterator++)
+                    if (iterator.has_value() && c.has_value() && iterator.value() == c.value()) {
+                        return iterator.value();
+                    }
+                return std::nullopt;
+            }
+
+        private:
+            /**
+             * Private member _data represented by a std::vector<std::optional<Component>> the data of the component
+             */
+            container_t _data;
+    };
+}
 
 #endif /* !SPARSEARRAY_HPP_ */
