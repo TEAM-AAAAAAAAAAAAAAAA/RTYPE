d6abc6ebcb2a838e4f4c562f0aa398b556af9dde
hourcadettej <julien.hourcadette@live.fr>
2022-10-02 18:54:34 +0200

    misc: update main.cpp testing ecs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "ecs/Registry.hpp"
+#include "ecs/components/Position.hpp"
 
 int main()
 {
+    ecs::Registry r;
+    ecs::SparseArray<ecs::Position> p;
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
