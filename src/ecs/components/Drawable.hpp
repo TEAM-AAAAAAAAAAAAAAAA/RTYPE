32a1c572f360d846e1df274793569beaefe0c151
aurelenc <aurele.nicolas@epitech.eu>
2022-10-03 18:40:00 +0200

    feat: add component drawable

---
+++ src/ecs/components/Drawable.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "SFML/Graphics/Texture.hpp"
+
+namespace ecs::component
+{
+    struct Drawable {
+        Drawable(sf::Texture texture) : Texture(texture) {}
+
+        sf::Texture Texture;
+    };
+} // namespace ecs::componenté&&é
