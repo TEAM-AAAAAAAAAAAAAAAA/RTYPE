a48d4a2efb5a4cccf71e5726923583a71831f32b
aurelenc <aurele.nicolas@epitech.eu>
2022-10-03 19:49:49 +0200

    feat: add ecs demo

--- src/demo/Demo.cpp
+++ src/demo/Demo.cpp
@@ -5,10 +5,23 @@
 ** rtype
 */
 
+#include "../ecs/Registry.hpp"
+#include "../ecs/components/Position.hpp"
+#include "../ecs/components/Velocity.hpp"
+#include "../ecs/systems/Movement.hpp"
+#include "../ecs/systems/PositionLogger.hpp"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World ! " << std::endl;
+    ecs::Registry r;
+    ecs::Entity e = r.spawn_entity();
+    r.emplaceComponent<ecs::component::Position>(e, 1, 2);
+    r.emplaceComponent<ecs::component::Velocity>(e, 1, 2);
+    while (true)
+    {
+        ecs::systems::movement(r);
+        ecs::systems::positionLogger(r);
+    }
     return 0;
 }
