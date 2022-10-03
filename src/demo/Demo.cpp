aa78600d0d64b1e0b627382b007b36314986050b
hourcadettej <julien.hourcadette@live.fr>
2022-10-03 18:53:00 +0200

    misc: set up rtype demo, folder & executable

---
+++ src/demo/Demo.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, by hourcadettej on 10/3/22.
+** rtype
+** File description:
+** rtype
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World ! " << std::endl;
+    return 0;
+}
