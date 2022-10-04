35a715eeca8c3d641fc04015071f80c4a800ef0d
aurelenc <aurele.nicolas@epitech.eu>
2022-10-04 19:28:14 +0200

    feat: add other constructors to drawable component

--- src/ecs/components/Drawable.hpp
+++ src/ecs/components/Drawable.hpp
@@ -1,3 +1,15 @@
+/*
+ * File: Drawable.hpp
+ * Project: components
+ * File Created: Monday, 3rd October 2022 6:32:42 pm
+ * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
+ * -----
+ * Last Modified: Tuesday, 4th October 2022 7:22:28 pm
+ * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
+ * -----
+ * Copyright 2022 - 2022 Your Company, Your Company
+ */
+
 #pragma once
 
 #include "SFML/Graphics/Texture.hpp"
@@ -5,8 +17,9 @@
 namespace ecs::component
 {
     struct Drawable {
-        Drawable(sf::Texture texture) : Texture(texture) {}
         Drawable(std::string texture) { Texture.loadFromFile(texture); }
+        Drawable(std::string texture, sf::IntRect rect) { Texture.loadFromFile(texture, rect); }
+        Drawable(sf::Texture texture) : Texture(texture) {}
 
         sf::Texture Texture;
     };
