4e225c1b6916cf7d1e4c5f34cab09bcd21c6db32
aurelenc <aurele.nicolas@epitech.eu>
2022-10-05 00:05:43 +0200

    feat: add bullet and health components

---
+++ src/ecs/components/Bullet.hpp
@@ -0,0 +1,20 @@
+/*
+ * File: Bullet.hpp
+ * Project: components
+ * File Created: Tuesday, 4th October 2022 11:59:32 pm
+ * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
+ * -----
+ * Last Modified: Wednesday, 5th October 2022 12:00:27 am
+ * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
+ * -----
+ * Copyright 2022 - 2022 Your Company, Your Company
+ */
+
+#pragma once
+
+namespace ecs::component
+{
+    struct Bullet {
+        int damage;
+    };
+} // namespace ecs::component
---
+++ src/ecs/components/Health.hpp
@@ -0,0 +1,20 @@
+/*
+ * File: Health.hpp
+ * Project: components
+ * File Created: Wednesday, 5th October 2022 12:01:56 am
+ * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
+ * -----
+ * Last Modified: Wednesday, 5th October 2022 12:02:27 am
+ * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
+ * -----
+ * Copyright 2022 - 2022 Your Company, Your Company
+ */
+
+#pragma once
+
+namespace ecs::component
+{
+    struct Health {
+        int health;
+    };
+} // namespace ecs::component
