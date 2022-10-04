046937d94904c0fb389fab8e1ac34a0e23c28c46
aurelenc <aurele.nicolas@epitech.eu>
2022-10-04 19:29:03 +0200

    feat: add Direction component

---
+++ src/ecs/components/Direction.hpp
@@ -0,0 +1,21 @@
+/*
+ * File: Direction.hpp
+ * Project: components
+ * File Created: Tuesday, 4th October 2022 7:10:05 pm
+ * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
+ * -----
+ * Last Modified: Tuesday, 4th October 2022 7:22:19 pm
+ * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
+ * -----
+ * Copyright 2022 - 2022 Your Company, Your Company
+ */
+
+#pragma once
+
+namespace ecs::component
+{
+    struct Direction {
+        int x;
+        int y;
+    };
+} // namespace ecs::component
