b74c13a8521916a549611e49950aec23fc882513
aurelenc <aurele.nicolas@epitech.eu>
2022-10-05 00:37:11 +0200

    rtor: bullet component is now damage

---
+++ src/ecs/components/Damage.hpp
@@ -0,0 +1,20 @@
+/*
+ * File: Damage.hpp
+ * Project: components
+ * File Created: Tuesday, 4th October 2022 11:59:32 pm
+ * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
+ * -----
+ * Last Modified: Wednesday, 5th October 2022 12:16:48 am
+ * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
+ * -----
+ * Copyright 2022 - 2022 Your Company, Your Company
+ */
+
+#pragma once
+
+namespace ecs::component
+{
+    struct Damage {
+        int damage;
+    };
+} // namespace ecs::component
