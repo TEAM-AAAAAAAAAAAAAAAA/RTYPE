5deb94c04540f7a032495702e58839139d4b3de4
aurelenc <aurele.nicolas@epitech.eu>
2022-10-04 22:25:18 +0200

    refactor: simplify event handling systems

--- src/ecs/systems/HandleSFMLEvents.hpp
+++ src/ecs/systems/HandleSFMLEvents.hpp
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 4th October 2022 7:34:35 pm
  * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
  * -----
- * Last Modified: Tuesday, 4th October 2022 10:11:59 pm
+ * Last Modified: Tuesday, 4th October 2022 10:24:43 pm
  * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
  * -----
  * Copyright 2022 - 2022 Your Company, Your Company
@@ -13,7 +13,6 @@
 #pragma once
 
 #include <functional>
-#include <iostream>
 #include "World.hpp"
 #include "components/Controllable.hpp"
 
@@ -21,20 +20,12 @@ namespace ecs::systems
 {
     std::function<void(World &)> handleSFMLEvents = [](World &world) {
         sf::Event event;
-        auto const &controllables = world.registry.getComponents<component::Controllable>();
 
-        for (size_t i = 0; i < controllables.size(); ++i) {
-            auto &contr = controllables[i];
-
-            if (contr) {
-                auto const con = contr.value();
-                while (world.getWindow().pollEvent(event)) {
-                    switch (event.type) {
-                        case sf::Event::Closed: world.getWindow().close(); break;
-                        case sf::Event::KeyPressed: break;
-                        default: break;
-                    }
-                }
+        while (world.getWindow().pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed: world.getWindow().close(); break;
+                case sf::Event::KeyPressed: break;
+                default: break;
             }
         }
     };
--- src/ecs/systems/HandleSFMLMovements.hpp
+++ src/ecs/systems/HandleSFMLMovements.hpp
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 4th October 2022 10:11:08 pm
  * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
  * -----
- * Last Modified: Tuesday, 4th October 2022 10:16:24 pm
+ * Last Modified: Tuesday, 4th October 2022 10:24:56 pm
  * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
  * -----
  * Copyright 2022 - 2022 Your Company, Your Company
@@ -13,34 +13,26 @@
 #pragma once
 
 #include <functional>
-#include <iostream>
 #include "World.hpp"
 #include "components/Controllable.hpp"
 
 namespace ecs::systems
 {
     std::function<void(World &)> handleSFMLMovements = [](World &world) {
-        struct Movement {
-            bool goUp;
-            bool goLeft;
-            bool goDown;
-            bool goRight;
-        };
         auto const &controllables = world.registry.getComponents<component::Controllable>();
 
         for (size_t i = 0; i < controllables.size(); ++i) {
             auto &contr = controllables[i];
 
             if (contr) {
-                auto const con = contr.value();
-                if (sf::Keyboard::isKeyPressed(con.MoveUp) || sf::Keyboard::isKeyPressed(con.MoveUpSecondary))
+                if (sf::Keyboard::isKeyPressed(contr.value().MoveUp) || sf::Keyboard::isKeyPressed(contr.value().MoveUpSecondary))
                     world.pushEvent(ecs::Event(ecs::Event::EventType::MoveUp));
-                else if (sf::Keyboard::isKeyPressed(con.MoveLeft) || sf::Keyboard::isKeyPressed(con.MoveLeftSecondary))
+                else if (sf::Keyboard::isKeyPressed(contr.value().MoveLeft) || sf::Keyboard::isKeyPressed(contr.value().MoveLeftSecondary))
                     world.pushEvent(ecs::Event(ecs::Event::EventType::MoveLeft));
-                else if (sf::Keyboard::isKeyPressed(con.MoveDown) || sf::Keyboard::isKeyPressed(con.MoveDownSecondary))
+                else if (sf::Keyboard::isKeyPressed(contr.value().MoveDown) || sf::Keyboard::isKeyPressed(contr.value().MoveDownSecondary))
                     world.pushEvent(ecs::Event(ecs::Event::EventType::MoveDown));
-                else if (sf::Keyboard::isKeyPressed(con.MoveRight)
-                    || sf::Keyboard::isKeyPressed(con.MoveRightSecondary))
+                else if (sf::Keyboard::isKeyPressed(contr.value().MoveRight)
+                    || sf::Keyboard::isKeyPressed(contr.value().MoveRightSecondary))
                     world.pushEvent(ecs::Event(ecs::Event::EventType::MoveRight));
             }
         }
