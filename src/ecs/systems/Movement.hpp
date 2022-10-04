0e46a06e5d41e5ea03627b548008a7d1ea53627e
aurelenc <aurele.nicolas@epitech.eu>
2022-10-04 23:39:51 +0200

    feat: add direction to movement

--- src/ecs/systems/Movement.hpp
+++ src/ecs/systems/Movement.hpp
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 4th October 2022 6:33:43 pm
  * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
  * -----
- * Last Modified: Tuesday, 4th October 2022 7:27:10 pm
+ * Last Modified: Tuesday, 4th October 2022 11:37:20 pm
  * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
  * -----
  * Copyright 2022 - 2022 Your Company, Your Company
@@ -15,6 +15,7 @@
 #include <functional>
 #include <iostream>
 #include "World.hpp"
+#include "components/Direction.hpp"
 #include "components/Position.hpp"
 #include "components/Velocity.hpp"
 
@@ -23,16 +24,20 @@ namespace ecs::systems
     std::function<void(World &)> movement = [](World &world) {
         auto &positions = world.registry.getComponents<component::Position>();
         auto const &velocities = world.registry.getComponents<component::Velocity>();
+        auto &directions = world.registry.getComponents<component::Direction>();
 
         static sf::Clock clock;
         for (size_t i = 0; i < positions.size() && i < velocities.size(); ++i) {
             auto &pos = positions[i];
             auto const &vel = velocities[i];
+            auto &dir = directions[i];
             if (pos && vel) {
-                if (clock.getElapsedTime().asMilliseconds() > 50) {
-                    pos.value().x += vel.value().x;
-                    pos.value().y += vel.value().y;
+                if (clock.getElapsedTime().asMilliseconds() > 10) {
+                    pos.value().x += vel.value().x * (dir.value().x);
+                    pos.value().y += vel.value().y * dir.value().y;
                     clock.restart();
+                    dir.value().x = 0;
+                    dir.value().y = 0;
                 }
             }
         };
