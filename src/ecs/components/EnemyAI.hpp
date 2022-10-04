25438627021116a11e9c19464a38b0375294fd35
aurelenc <aurele.nicolas@epitech.eu>
2022-10-04 19:28:53 +0200

    feat: add EnemyAI component

---
+++ src/ecs/components/EnemyAI.hpp
@@ -0,0 +1,20 @@
+/*
+ * File: EnemyAI.hpp
+ * Project: components
+ * File Created: Tuesday, 4th October 2022 6:59:11 pm
+ * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
+ * -----
+ * Last Modified: Tuesday, 4th October 2022 7:26:25 pm
+ * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
+ * -----
+ * Copyright 2022 - 2022 Your Company, Your Company
+ */
+
+#pragma once
+
+namespace ecs::component
+{
+    struct EnemyAI {
+        EnemyAI() {}
+    };
+} // namespace ecs::component
