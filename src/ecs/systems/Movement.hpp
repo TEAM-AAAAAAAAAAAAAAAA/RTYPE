57047a1866d62cde8dd57da32ac12fc8f7d78990
aurelenc <aurele.nicolas@epitech.eu>
2022-10-04 19:27:24 +0200

    fix: add clock to movement system

--- src/ecs/systems/Movement.hpp
+++ src/ecs/systems/Movement.hpp
@@ -1,3 +1,15 @@
+/*
+ * File: Movement.hpp
+ * Project: systems
+ * File Created: Tuesday, 4th October 2022 6:33:43 pm
+ * Author: Aurèle Nicolas (aurele.nicolas@epitech.eu)
+ * -----
+ * Last Modified: Tuesday, 4th October 2022 7:27:10 pm
+ * Modified By: Aurèle Nicolas (aurele.nicolas@epitech.eu>)
+ * -----
+ * Copyright 2022 - 2022 Your Company, Your Company
+ */
+
 #pragma once
 
 #include <functional>
@@ -12,12 +24,16 @@ namespace ecs::systems
         auto &positions = world.registry.getComponents<component::Position>();
         auto const &velocities = world.registry.getComponents<component::Velocity>();
 
+        static sf::Clock clock;
         for (size_t i = 0; i < positions.size() && i < velocities.size(); ++i) {
             auto &pos = positions[i];
             auto const &vel = velocities[i];
             if (pos && vel) {
-                pos.value().x += vel.value().x;
-                pos.value().y += vel.value().y;
+                if (clock.getElapsedTime().asMilliseconds() > 50) {
+                    pos.value().x += vel.value().x;
+                    pos.value().y += vel.value().y;
+                    clock.restart();
+                }
             }
         };
     };
