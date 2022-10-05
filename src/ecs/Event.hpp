71f87d486f9e93132e684e15f472c7ec51a763ec
hourcadettej <julien.hourcadette@live.fr>
2022-10-05 02:08:21 +0200

    docs: add event's documentation

--- src/ecs/Event.hpp
+++ src/ecs/Event.hpp
@@ -14,40 +14,56 @@
 
 namespace ecs
 {
+    /**
+     * Event class is used to manage every events gave by the user
+     * The Event class make the link between the user and the back-end
+     */
     class Event {
-      public:
-        enum EventType {
-            Null,
-            Unknown,
-            Quit,
-            MoveUp,
-            MoveLeft,
-            MoveDown,
-            MoveRight,
-            Shoot,
-            UIUp,
-            UILeft,
-            UIDown,
-            UIRight,
-            UIEnter,
-        };
-        Event(EventType e) : _e(e) {}
+        public:
+            /**
+             * List every type of event currently managed
+             */
+            enum EventType {
+                Null,
+                Unknown,
+                Quit,
+                MoveUp,
+                MoveLeft,
+                MoveDown,
+                MoveRight,
+                Shoot,
+                UIUp,
+                UILeft,
+                UIDown,
+                UIRight,
+                UIEnter,
+            };
+            /**
+             * Default constructor is deleted because you have to inform the event you want to treat
+             */
+            Event() = delete;
 
-        bool operator==(const Event other) const
-        {
-            if (_e == other._e)
-                return true;
-            return false;
-        }
+            /**
+             * Constructor of Event class
+             * @param e The Event you want to treat
+             */
+            Event(EventType e) : _e(e) {}
 
-        bool operator==(const Event::EventType type) const
-        {
-            if (_e == type)
-                return true;
-            return false;
-        }
+            bool operator==(const Event other) const
+            {
+                if (_e == other._e)
+                    return true;
+                return false;
+            }
 
-      private:
-        const EventType _e;
+            bool operator==(const Event::EventType type) const
+            {
+                if (_e == type)
+                    return true;
+                return false;
+            }
+
+        private:
+            const EventType _e;
     };
 } // namespace ecs
