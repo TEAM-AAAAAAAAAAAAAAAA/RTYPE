521e69786df0a47058a56d5ed1d4327fdae9c9b9
hourcadettej <julien.hourcadette@live.fr>
2022-10-05 02:07:19 +0200

    docs: add Engine.hpp documentation

--- src/ecs/Engine.hpp
+++ src/ecs/Engine.hpp
@@ -32,74 +32,110 @@
 
 namespace ecs
 {
+    /**
+     * The Engine class manages the whole part of systems in ecs in a given world.
+     * Systems are, most of the time, short function using components to realise some actions.
+     * For example, to make an Entity move, we need a component Position and a component Velocity,
+     * the system is the thing who update the position of the Entity according to the Velocity component
+     * by adding velocity value to the x & y member of the component position of the Entity.
+     */
     class Engine {
-      public:
-        Engine(int wSizeX = 800, int wSizeY = 600, std::string wTitle = "r-type") : _worldSwitchReady(false)
-        {
-            _window = std::make_unique<sf::RenderWindow>(sf::VideoMode(wSizeX, wSizeY), wTitle);
-            _window.get()->setFramerateLimit(60);
-            ecs::World initWorld(_window);
-            ecs::Entity player = initWorld.registry.spawn_entity();
-            initWorld.registry.addComponent<ecs::component::Position>(player, {10, 10});
-            initWorld.registry.addComponent<ecs::component::Velocity>(player, {5, 5});
-            initWorld.registry.addComponent<ecs::component::Size>(player, {2, 2});
-            initWorld.registry.addComponent<ecs::component::Direction>(player, {0, 0});
-            initWorld.registry.addComponent<ecs::component::Drawable>(
-                player, {"src/demo/assets/textures/players.gif", {1, 1, 32, 16}});
-            initWorld.registry.addComponent<ecs::component::Controllable>(
-                player, {sf::Keyboard::Z, sf::Keyboard::Q, sf::Keyboard::S, sf::Keyboard::D});
+        public:
+            /**
+             * Constructor of Engine class, it takes care of register every component in the world's registry.
+             * It also create a player and an enemy.
+             * And finally it takes care of adding every systems related to the components in the initWorld.
+             * @param wSizeWidth width size of the window in pixel
+             * @param wSizeHeight height size of the window in pixel
+             * @param wTitle window's title, "r-type" as default
+             */
+            explicit Engine(int wSizeWidth = 800, int wSizeHeight = 600, std::string wTitle = "r-type") : _worldSwitchReady(false)
+            {
+                _window = std::make_unique<sf::RenderWindow>(sf::VideoMode(wSizeWidth, wSizeHeight), wTitle);
+                _window.get()->setFramerateLimit(60);
+                ecs::World initWorld(_window);
+                ecs::Entity player = initWorld.registry.spawn_entity();
+                initWorld.registry.addComponent<ecs::component::Position>(player, {10, 10});
+                initWorld.registry.addComponent<ecs::component::Velocity>(player, {5, 5});
+                initWorld.registry.addComponent<ecs::component::Size>(player, {2, 2});
+                initWorld.registry.addComponent<ecs::component::Direction>(player, {0, 0});
+                initWorld.registry.addComponent<ecs::component::Drawable>(
+                        player, {"src/demo/assets/textures/players.gif", {1, 1, 32, 16}});
+                initWorld.registry.addComponent<ecs::component::Controllable>(
+                        player, {sf::Keyboard::Z, sf::Keyboard::Q, sf::Keyboard::S, sf::Keyboard::D});
 
-            ecs::Entity enemy = initWorld.registry.spawn_entity();
-            initWorld.registry.addComponent<ecs::component::Position>(enemy, {500, 500});
-            initWorld.registry.addComponent<ecs::component::Size>(enemy, {5, 5});
-            initWorld.registry.addComponent<ecs::component::Drawable>(
-                enemy, {"src/demo/assets/textures/players.gif", {1, 18, 32, 16}});
-            initWorld.registry.addComponent<ecs::component::EnemyAI>(enemy, {});
+                ecs::Entity enemy = initWorld.registry.spawn_entity();
+                initWorld.registry.addComponent<ecs::component::Position>(enemy, {500, 500});
+                initWorld.registry.addComponent<ecs::component::Size>(enemy, {5, 5});
+                initWorld.registry.addComponent<ecs::component::Drawable>(
+                        enemy, {"src/demo/assets/textures/players.gif", {1, 18, 32, 16}});
+                initWorld.registry.addComponent<ecs::component::EnemyAI>(enemy, {});
 
-            initWorld.addSystem(ecs::systems::handleSFMLEvents);
-            initWorld.addSystem(ecs::systems::handleSFMLMovements);
-            initWorld.addSystem(ecs::systems::manageClientEvents);
-            // initWorld.addSystem(ecs::systems::positionLogger);
-            initWorld.addSystem(ecs::systems::draw);
-            initWorld.addSystem(ecs::systems::movement);
+                initWorld.addSystem(ecs::systems::handleSFMLEvents);
+                initWorld.addSystem(ecs::systems::handleSFMLMovements);
+                initWorld.addSystem(ecs::systems::manageClientEvents);
+                // initWorld.addSystem(ecs::systems::positionLogger);
+                initWorld.addSystem(ecs::systems::draw);
+                initWorld.addSystem(ecs::systems::movement);
 
-            _currentWorld = std::make_unique<ecs::World>(initWorld);
-        }
-
-        ecs::World &getWorld() { return *_currentWorld; }
-
-        void setWaitingWorld(ecs::World &world)
-        {
-            if (_waitingWorld)
-                _waitingWorld.release();
-            _waitingWorld = std::make_unique<ecs::World>(world);
-        }
-
-        void setWorldSwitchReady() { _worldSwitchReady = true; }
-
-        void run()
-        {
-            while (_window.get()->isOpen()) {
-                _currentWorld.get()->runSystems();
-                if (isWorldSwitchReady())
-                    switchWorlds();
+                _currentWorld = std::make_unique<ecs::World>(initWorld);
             }
-        }
 
-        // Entity createEntity() { return _currentWorld.get()->registry.spawn_entity(); }
+            /**
+             * Default constructor deleted because you need to register width and height of the window you want to create
+             */
+            Engine() = delete;
 
-      private:
-        std::unique_ptr<sf::RenderWindow> _window;
-        std::unique_ptr<ecs::World> _currentWorld;
-        std::unique_ptr<ecs::World> _waitingWorld;
-        bool _worldSwitchReady;
+            /**
+             * Used to get the currentWorld currently used by the ecs
+             * @return The currently used world by the ecs
+             */
+            ecs::World &getWorld() { return *_currentWorld; }
 
-        bool isWorldSwitchReady() { return _worldSwitchReady; }
+            /**
+             * Set the world given as parameter to release mode
+             * @param world The world you want to set to release mode
+             */
+            void setWaitingWorld(ecs::World &world)
+            {
+                if (_waitingWorld)
+                    _waitingWorld.release();
+                _waitingWorld = std::make_unique<ecs::World>(world);
+            }
 
-        void switchWorlds()
-        {
-            if (_waitingWorld)
-                _currentWorld.swap(_waitingWorld);
-        }
+            /**
+             * Set the world given as parameter ready to switch for another one
+             */
+            void setWorldSwitchReady() { _worldSwitchReady = true; }
+
+            /**
+             * Main loop of the current world
+             * Actually run while the window is open
+             */
+            void run()
+            {
+                while (_window->isOpen()) {
+                    _currentWorld->runSystems();
+                    if (isWorldSwitchReady())
+                        switchWorlds();
+                }
+            }
+
+        private:
+            /**
+             * The window used to display every drawable component
+             */
+            std::unique_ptr<sf::RenderWindow> _window;
+            std::unique_ptr<ecs::World> _currentWorld;
+            std::unique_ptr<ecs::World> _waitingWorld;
+            bool _worldSwitchReady;
+
+            [[nodiscard]] bool isWorldSwitchReady() const { return _worldSwitchReady; }
+
+            void switchWorlds()
+            {
+                if (_waitingWorld)
+                    _currentWorld.swap(_waitingWorld);
+            }
     };
 } // namespace ecs
