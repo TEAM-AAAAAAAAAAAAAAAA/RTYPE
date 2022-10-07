86b578522028cf94942a841c208ee156ad89a489
Broke Machine <emile.ettel@epitech.eu>
2022-10-07 17:10:46 +0200

    feat: added networkId component, it allows us to indicate the entity when conversing with the client

---
+++ src/ecs/components/NetworkId.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, by hourcadettej on 10/3/22.
+** rtype
+** File description:
+** rtype
+*/
+
+#pragma once
+#include <iostream>
+
+namespace ecs::component
+{
+    struct NetworkId {
+        size_t id;
+    };
+} // namespace ecs::component
