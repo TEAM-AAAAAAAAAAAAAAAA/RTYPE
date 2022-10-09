a2f9e3954567e64e7055766f84f7f19314179d82
hourcadettej <julien.hourcadette@live.fr>
2022-10-09 19:20:40 +0200

    docs: update ManageClientEvents documentation and remove heavy if statement

--- src/ecs/systems/ManageClientEvents.hpp
+++ src/ecs/systems/ManageClientEvents.hpp
@@ -22,6 +22,10 @@ namespace ecs::systems
     std::function<void(World &)> manageClientEvents = [](World &world) {
         while (world.getEvent() != ecs::Event::EventType::Null) {
 
+/**
+ * Region only reserved for player movement.
+ * Incrementing and decrementing the entity direction is used to manage multi input in the sametime, still caped as 1 or -1
+ */
 #pragma region Player Movement
             if (world.getEvent() == ecs::Event::EventType::MoveUp || world.getEvent() == ecs::Event::EventType::MoveLeft
                 || world.getEvent() == ecs::Event::EventType::MoveDown
@@ -34,26 +38,22 @@ namespace ecs::systems
                     auto const &con = controllables[i];
                     if (dir && con) {
                         if (world.getEvent() == ecs::Event::EventType::MoveUp)
-                            dir.value().y -= 1;
+                            dir.value().y <= 0 ? dir.value().y = -1 : dir.value().y -= 1;
                         else if (world.getEvent() == ecs::Event::EventType::MoveLeft)
-                            dir.value().x -= 1;
+                            dir.value().x <= 0 ? dir.value().x = -1 : dir.value().x -=1;
                         else if (world.getEvent() == ecs::Event::EventType::MoveDown)
-                            dir.value().y += 1;
+                            dir.value().y >= 0 ? dir.value().y = 1 : dir.value().y += 1;
                         else if (world.getEvent() == ecs::Event::EventType::MoveRight)
-                            dir.value().x += 1;
-                        if (dir.value().x > 0)
-                            dir.value().x = 1;
-                        else if (dir.value().x < 0)
-                            dir.value().x = -1;
-                        if (dir.value().y > 0)
-                            dir.value().y = 1;
-                        else if (dir.value().y < 0)
-                            dir.value().y = -1;
+                            dir.value().x >= 0 ? dir.value().x = 1 : dir.value().x += 1;
                     }
-                };
+                }
             }
 #pragma endregion
 
+/**
+ * Region only reserved for bullet Shoot
+ * If the system is called, instantly create a new entity with bullet's component
+ */
 #pragma region Bullet Shoot
             else if (world.getEvent() == ecs::Event::EventType::Shoot) {
                 auto const &controllables = world.registry.getComponents<component::Controllable>();
@@ -64,7 +64,7 @@ namespace ecs::systems
                     auto const &con = controllables[i];
                     if (pos && con) {
                         std::filesystem::path playerPath =
-                            ecs::crossPlatformPath("src", "demo", "assets", "textures", "players.gif");
+                                ecs::crossPlatformPath("src", "demo", "assets", "textures", "players.gif");
                         ecs::Entity bullet = world.registry.spawn_entity();
                         world.registry.addComponent<ecs::component::Direction>(bullet, {1, 0});
                         world.registry.addComponent<ecs::component::Drawable>(bullet, {playerPath, {5, 5, 1, 1}});
