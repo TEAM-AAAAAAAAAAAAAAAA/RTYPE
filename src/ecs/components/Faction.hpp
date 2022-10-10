8d9aa60ec2568ca37c615d1fc602d6ba4ff123d1
aurelenc <aurele.nicolas@epitech.eu>
2022-10-10 19:51:03 +0200

    feat: add factions

---
+++ src/ecs/components/Faction.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** RTYPE
+** File description:
+** Faction
+*/
+
+#pragma once
+
+namespace ecs::component
+{
+    struct Faction {
+        enum Factions {
+            None,
+            Players,
+            Enemies,
+        };
+
+        Faction(Factions faction = Factions::None) : faction(faction) {}
+
+        Factions faction;
+    };
+} // namespace ecs::component
