322274d04b08a45cac33d3affd1f19bc192055e5
Guillaume <guillaume.terriere@epitech.eu>
2022-10-11 11:20:35 +0200

    feat: death of entity if life under 0

--- src/ecs/systems/DeathUpdate.hpp
+++ src/ecs/systems/DeathUpdate.hpp
@@ -13,20 +13,19 @@
 #include "Network.hpp"
 #include "Position.hpp"
 #include "NetworkId.hpp"
-#include "Size.hpp"
-#include "EntityType.hpp"
+#include "Constant.hpp"
+#include "EntityLife.hpp"
 
 namespace ecs::systems
 {
     /**
-     * System used to push updated position of entity to the server outgoing queue.
+     * System used to push death of entity to the server outgoing queue.
      */
     std::function<void(World &)> serverPush = [](World &world) {
         auto &network = world.registry.getComponents<component::Network>();
         auto &position = world.registry.getComponents<component::Position>();
+        auto &life = world.registry.getComponents<component::EntityLife>();
         auto &networkId = world.registry.getComponents<component::NetworkId>();
-        auto &entityType = world.registry.getComponents<component::EntityType>();
-        auto &size = world.registry.getComponents<component::Size>();
         auto &serv = network[0];
 
         for (size_t i = 0; i < network.size() && !serv; i++)
@@ -35,27 +34,22 @@ namespace ecs::systems
             std::cerr << "Error: Server component not found" << std::endl;
             return;
         }
-        for (size_t i = 0; i < position.size() && i < networkId.size() && i < size.size(); i ++) {
+        for (size_t i = 0; i < position.size() && i < networkId.size(); i ++) {
             auto &pos = position[i];
-            auto &size = size[i];
             auto &id = networkId[i];
-            auto &type = entityType[i];
-            if (pos && id) {
+            auto &li = life[i];
+            if (li.life <= 0 && pos && id) {
                 std::array<char, 2> idBin = id.serialize();
                 std::array<char, 4> posBin = pos.serialize();
-                std::array<char, 2> sizeBin = size.serialize();
-                std::array<char, 8> msg;
+                std::array<char, 7> msg;
                 msg[0] = idBin[0];
                 msg[1] = idBin[1];
-                msg[2] = type.type;
-                msg[3] = posBin[0];
-                msg[4] = posBin[1];
-                msg[5] = posBin[2];
-                msg[6] = posBin[3];
-                msg[7] = sizeBin[0];
-                msg[8] = sizeBin[1];
-                msg[9] = ecs::getPacketTypeKey(ecs::ENTITY_MOVE);;
-                serv.serv.outgoingQueue.push(Network::ServerMessage(msg, std::vector<unsigned int>()));
+                msg[2] = posBin[0];
+                msg[3] = posBin[1];
+                msg[4] = posBin[2];
+                msg[5] = posBin[3];
+                msg[6] = ecs::getPacketTypeKey(ecs::ENTITY_DEATH);
+                serv.push(msg); // This is to be replaced when possible, it will not work nor should it
             }
         }
     };
