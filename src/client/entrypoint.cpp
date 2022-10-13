a3387265e640015f2f1ea280b1495eea68d64609
aurelenc <aurele.nicolas@epitech.eu>
2022-10-13 14:46:20 +0200

    fix: merge with main

--- src/client/entrypoint.cpp
+++ src/client/entrypoint.cpp
@@ -12,7 +12,7 @@
 #include "components/Direction.hpp"
 #include "components/Drawable.hpp"
 #include "components/EnemyAI.hpp"
-// #include "components/Faction.hpp"
+#include "components/Faction.hpp"
 #include "components/Health.hpp"
 #include "components/Position.hpp"
 #include "components/Size.hpp"
@@ -24,7 +24,7 @@
 #include "systems/ManageClientEvents.hpp"
 #include "systems/Movement.hpp"
 #include "systems/PositionLogger.hpp"
-// #include "systems/ProjectileCollision.hpp"
+#include "systems/ProjectileCollision.hpp"
 
 ecs::World getGameWorld(ecs::Engine &engine)
 {
@@ -38,19 +38,19 @@ ecs::World getGameWorld(ecs::Engine &engine)
     world.registry.addComponent<ecs::component::Velocity>(player, {5, 5});
     world.registry.addComponent<ecs::component::Size>(player, {32, 64});
     world.registry.addComponent<ecs::component::Direction>(player, {1, 0});
-    world.registry.addComponent<ecs::component::Weapon>(player, {100, 10});
+    world.registry.addComponent<ecs::component::Weapon>(player, {100, 10, 10});
     world.registry.addComponent<ecs::component::Health>(player, {100});
-    // world.registry.addComponent<ecs::component::Faction>(player, {ecs::component::Faction::Factions::Players});
+    world.registry.addComponent<ecs::component::Faction>(player, {ecs::component::Faction::Factions::Players});
     // world.addSystem(ecs::systems::positionLogger);
     world.addSystem(ecs::systems::movement);
-    // world.addSystem(ecs::systems::projectileCollision);
+    world.addSystem(ecs::systems::projectileCollision);
 
     ecs::Entity enemy = world.registry.spawn_entity();
     world.registry.addComponent<ecs::component::Position>(enemy, {500, 500});
     world.registry.addComponent<ecs::component::Size>(enemy, {64, 128});
     world.registry.addComponent<ecs::component::EnemyAI>(enemy, {});
     world.registry.addComponent<ecs::component::Health>(enemy, {100});
-    // world.registry.addComponent<ecs::component::Faction>(enemy, {ecs::component::Faction::Factions::Enemies});
+    world.registry.addComponent<ecs::component::Faction>(enemy, {ecs::component::Faction::Factions::Enemies});
 
     const std::filesystem::path &playerPath = ecs::crossPlatformPath("src", "demo", "assets", "textures", "players.gif");
     world.registry.addComponent<ecs::component::Controllable>(
