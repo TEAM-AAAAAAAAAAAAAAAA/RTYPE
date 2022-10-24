0c4dab4e6ec039762b5028d7b3ffbf0e65da6aea
hourcadettej <julien.hourcadette@live.fr>
2022-10-24 23:42:14 +0200

    fix: debug the collision between projectile & enemies + debug kill entity

--- src/ecs/systems/ProjectileCollision.hpp
+++ src/ecs/systems/ProjectileCollision.hpp
@@ -10,61 +10,50 @@
 namespace ecs::systems
 {
     std::function<void(World &)> projectileCollision = [](World &world) {
-        auto const &projectiles = world.registry.getComponents<component::Projectile>();
-        auto const &positions = world.registry.getComponents<component::Position>();
+      auto const &projectiles = world.registry.getComponents<component::Projectile>();
+      auto const &positions = world.registry.getComponents<component::Position>();
+      auto &healths = world.registry.getComponents<component::Health>();
+      auto const &sizes = world.registry.getComponents<component::Size>();
+      std::stack<size_t> entities_to_kill;
 
-#pragma region Proj out of map
-        for (size_t i = 0; i < projectiles.size() && i < positions.size(); ++i) {
-            auto const &proj = projectiles[i];
-            auto const &projPos = positions[i];
+      if (!projectiles.size() || !positions.size() || !healths.size() || !sizes.size())
+          return;
+      for (size_t i = 0; i < projectiles.size() && i < positions.size(); i++) {
+          auto const &proj = projectiles[i];
+          auto const &projPos = positions[i];
 
-            if (proj && projPos) {
-                if (projPos.value().x < 0 || projPos.value().x > ecs::constant::mapWidth || projPos.value().y < 0
-                    || projPos.value().y > ecs::constant::mapWidth) {
-                    world.registry.killEntity(world.registry.entityFromIndex(i));
-                }
-            }
-        };
-#pragma endregion
+          if (!proj || !projPos)
+              continue;
+          for (size_t j = 0; j < healths.size() && j < positions.size() && j < sizes.size(); j++) {
+              auto &victimHealth = healths[j];
+              auto const &victimPos = positions[j];
+              auto const &victimSize = sizes[j];
 
-#pragma region Hitreg
-        auto &healths = world.registry.getComponents<component::Health>();
-        auto const &factions = world.registry.getComponents<component::Faction>();
-        auto const &sizes = world.registry.getComponents<component::Size>();
-        bool killProj = false;
-        size_t projId = 0;
-
-        for (size_t i = 0; i < projectiles.size() && i < positions.size(); ++i) {
-            auto const &proj = projectiles[i];
-            auto const &projPos = positions[i];
-
-            if (proj && projPos) {
-                for (size_t j = 0; j < healths.size() && j < positions.size(); ++j) {
-                    auto &victimHealth = healths[j];
-                    auto const &victimPos = positions[j];
-                    auto const &victimSize = sizes[j];
-
-                    if (victimHealth && victimPos && victimSize) {
-                        if (i < factions.size() && j < factions.size())
-                            if (factions[i] && factions[j])
-                                if (factions[i].value().faction == factions[j].value().faction)
-                                    continue;
-                        if (projPos.value().x > victimPos.value().x
-                            && projPos.value().x < victimPos.value().x + victimSize.value().height
-                            && projPos.value().y > victimPos.value().y
-                            && projPos.value().x < victimPos.value().y + victimSize.value().height) {
-                            victimHealth.value().health -= proj.value().damage;
-                            victimHealth.value().health <= 0 ? victimHealth.value().health = 0 : 0;
-                            projId = i;
-                            killProj = true;
-                        }
-                    }
-                }
-            }
-        };
-
-        if (killProj)
-            world.registry.killEntity(world.registry.entityFromIndex(projId));
-#pragma endregion
+              if (victimHealth && victimPos && victimSize) {
+                  if (projPos.value().x > victimPos.value().x
+                      && projPos.value().x < victimPos.value().x + victimSize.value().width
+                      && projPos.value().y > victimPos.value().y
+                      && projPos.value().x < victimPos.value().y + victimSize.value().height) {
+                      victimHealth.value().health -= proj.value().damage;
+                      if (victimHealth.value().health <= 0)
+                          entities_to_kill.push(j);
+                      entities_to_kill.push(i);
+                  }
+              }
+          }
+          if (projPos.value().x < 0
+              || projPos.value().x > ecs::constant::mapWidth
+              || projPos.value().y < 0
+              || projPos.value().y > ecs::constant::mapHeight) {
+              std::cout << "KILL PROJ pos.x : " << projPos.value().x << " | pos.y : " << projPos.value().y << std::endl;
+              entities_to_kill.push(i);
+          }
+      }
+      while (!entities_to_kill.empty()) {
+//            std::cout << "bin size : " << entities_to_kill.size() << " | id : " << entities_to_kill.top() << std::endl;
+          world.registry.killEntity(world.registry.entityFromIndex(entities_to_kill.top()));
+          entities_to_kill.pop();
+      }
+//        std::cout << "[END] bin size : " << entities_to_kill.size() << std::endl;
     };
 } // namespace ecs::systems
