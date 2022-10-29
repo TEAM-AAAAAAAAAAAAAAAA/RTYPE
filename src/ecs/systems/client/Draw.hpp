230ce08fff25731695cd95d146ba76b123173efe
hourcadettej <julien.hourcadette@live.fr>
2022-10-29 18:27:03 +0200

    misc: draw hitbox

--- src/ecs/systems/client/Draw.hpp
+++ src/ecs/systems/client/Draw.hpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include "World.hpp"
 #include "components/client/Drawable.hpp"
+#include "components/client/HitBox.hpp"
 #include "components/Position.hpp"
 #include "components/Size.hpp"
 
@@ -28,12 +29,14 @@ namespace ecs::systems
         auto const &positions = world.registry.getComponents<component::Position>();
         auto const &sizes = world.registry.getComponents<component::Size>();
         auto const &drawables = world.registry.getComponents<component::Drawable>();
+        auto const &hitBoxes = world.registry.getComponents<component::Hitbox>();
 
         world.getWindow().clear();
         for (size_t i = 0; i < positions.size() && i < sizes.size() && i < drawables.size(); ++i) {
             auto const &pos = positions[i];
             auto const &size = sizes[i];
             auto const &draw = drawables[i];
+            auto const &hitBox = hitBoxes[i];
             if (pos && size && draw) {
                 sf::Sprite sprite;
                 sprite.setTexture(draw.value().Texture);
@@ -42,6 +45,16 @@ namespace ecs::systems
                 sprite.setScale(scaleX, scaleY);
                 sprite.setPosition({static_cast<float>(pos.value().x), static_cast<float>(pos.value().y)});
                 world.getWindow().draw(sprite);
+            }
+            if (hitBox && hitBox->enableHitBox) {
+                sf::RectangleShape hitBoxRec({0, 0});
+
+                hitBoxRec.setFillColor(sf::Color::Transparent);
+                hitBoxRec.setOutlineColor(sf::Color::Red);
+                hitBoxRec.setOutlineThickness(2);
+                hitBoxRec.setSize({(float)size->width, (float)size->height});
+                hitBoxRec.setPosition({(float)(positions[i]->x), (float)(positions[i]->y)});
+                world.getWindow().draw(hitBoxRec);
             }
         };
         world.getWindow().display();
