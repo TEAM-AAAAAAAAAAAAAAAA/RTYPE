3344634e01e1379a146f27c898587010f67eebd3
hourcadettej <julien.hourcadette@live.fr>
2022-11-05 17:36:22 +0100

    misc: delete the associtated key

--- src/ecs/components/client/Hitbox.hpp
+++ src/ecs/components/client/Hitbox.hpp
@@ -15,7 +15,7 @@ namespace ecs::component
      * Used to show a red rectangle shape around the entity to display its hitBox
      */
     struct Hitbox {
-        explicit Hitbox(sf::Keyboard::Key key) : enableHitBox(false), keyBoardKey(key) {}
+        explicit Hitbox() : enableHitBox(false){}
 
         /**
          * Toogle used to know if we need to enable or disable the HitBox
@@ -23,18 +23,9 @@ namespace ecs::component
         bool enableHitBox;
 
         /**
-         * The associated binding to show the hitBox
-         */
-        sf::Keyboard::Key keyBoardKey;
-
-        /**
          * Used to switch the enable/disable mode of the hitBox pressing H
          */
         inline void switchHitBox() { enableHitBox = !enableHitBox;};
 
-        /**
-         * Setter of the HitBox class
-         */
-         inline void setHitBoxKey(sf::Keyboard::Key key) {keyBoardKey = key;};
     };
 } // namespace ecs::component
