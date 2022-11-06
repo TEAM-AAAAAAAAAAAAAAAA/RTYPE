4cac480ec1ad490df3799562e1e62ff4b5a52977
hourcadettej <julien.hourcadette@live.fr>
2022-11-06 18:07:08 +0100

    misc: add sf::Color Window::Color(18, 32, 45, 255)

--- src/utils/Window.cpp
+++ src/utils/Window.cpp
@@ -4,5 +4,6 @@
 namespace utils
 {
     sf::RenderWindow Window::_Instance(sf::RenderWindow(sf::VideoMode(utils::constant::mapWidth, utils::constant::mapHeight), "r-type"));
+    sf::Color Window::Color(18, 32, 45, 255);
 } // namespace utils
 #endif
