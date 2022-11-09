9fe5c791d4a87991a26b3230f6e203e394ae3758
hourcadettej <julien.hourcadette@live.fr>
2022-11-09 18:43:39 +0100

    feat: fix getGameWorld prototype in GetWorld.hpp

--- src/client/GetWorld.hpp
+++ src/client/GetWorld.hpp
@@ -9,5 +9,5 @@
 
 #include "World.hpp"
 
-ecs::World getGameWorld(const std::string& port, std::string host);
+ecs::World getGameWorld(const std::string& port, const std::string &host);
 ecs::World getMenuWorld();
