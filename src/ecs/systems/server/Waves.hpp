bbc41d8bea8cb2c3ffc3a3e8dfb7259d2d10b884
aurelenc <aurele.nicolas@epitech.eu>
2022-11-10 02:44:08 +0100

    feat: add janitor and alien factions to waves

--- src/ecs/systems/server/Waves.hpp
+++ src/ecs/systems/server/Waves.hpp
@@ -119,6 +119,128 @@ namespace ecs::systems
                         ecs::EnemyFactory::EnemyType::NoodleMonster, utils::constant::mapWidth - 200, 400,
                         ecs::component::Faction::Amalgam, ecs::component::MovementAI::AIType::LongUpDown);
                 }
+                if (waveCounter >= 11) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Scout,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::QuickUpDown);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Scout,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::ClockwiseSmall);
+                }
+                if (waveCounter >= 12) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Fighter,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::AntiClockwiseBig);
+                }
+                if (waveCounter >= 14) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Frigate,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::AntiClockwiseSmall);
+                }
+                if (waveCounter >= 15) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(),
+                        ecs::EnemyFactory::EnemyType::Battlecruiser,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::AntiClockwiseBig);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Frigate,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::LongLeftRight);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Fighter,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::LongLeftRight);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Fighter,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::Idle);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Scout,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::LongUpDown);
+                }
+                if (waveCounter >= 16) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(),
+                        ecs::EnemyFactory::EnemyType::Dreadnought,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::Idle);
+                }
+                if (waveCounter >= 17) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Torpedo,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::LongUpDown);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Torpedo,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::LongUpDown);
+                }
+                if (waveCounter >= 18) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Battlecruiser,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::ClockwiseSmall);
+                }
+                if (waveCounter >= 19) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(),
+                        ecs::EnemyFactory::EnemyType::Battlecruiser,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Janitor, ecs::component::MovementAI::AIType::AntiClockwiseBig);
+                }
+                if (waveCounter >= 21) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Scout,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::QuickUpDown);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Scout,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::ClockwiseSmall);
+                }
+                if (waveCounter >= 22) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Fighter,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::AntiClockwiseBig);
+                }
+                if (waveCounter >= 24) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Frigate,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::AntiClockwiseSmall);
+                }
+                if (waveCounter >= 25) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(),
+                        ecs::EnemyFactory::EnemyType::Battlecruiser,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::AntiClockwiseBig);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Frigate,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::LongLeftRight);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Fighter,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::LongLeftRight);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Fighter,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::Idle);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Scout,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::LongUpDown);
+                }
+                if (waveCounter >= 26) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(),
+                        ecs::EnemyFactory::EnemyType::Dreadnought,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::Idle);
+                }
+                if (waveCounter >= 27) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Torpedo,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::LongUpDown);
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Torpedo,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::LongUpDown);
+                }
+                if (waveCounter >= 28) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(), ecs::EnemyFactory::EnemyType::Battlecruiser,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::ClockwiseSmall);
+                }
+                if (waveCounter >= 29) {
+                    ecs::EnemyFactory::spawnEnemy(ecs::WorldManager::getWorld(),
+                        ecs::EnemyFactory::EnemyType::Battlecruiser,
+                        200 + std::rand() % (utils::constant::mapWidth - 600), std::rand() % utils::constant::mapHeight,
+                        ecs::component::Faction::Alien, ecs::component::MovementAI::AIType::AntiClockwiseBig);
+                }
             }
             lastWave = utils::constant::chrono::now().time_since_epoch().count();
         }
