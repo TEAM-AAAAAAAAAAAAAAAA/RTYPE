2d94748e61c0bd8780f97f8f4b93e44c36f56cf4
hourcadettej <julien.hourcadette@live.fr>
2022-11-11 20:38:51 +0100

    feat: delete test world & add mouseEvent

--- src/ecs/systems/client/HandleSFMLEvents.hpp
+++ src/ecs/systems/client/HandleSFMLEvents.hpp
@@ -14,24 +14,6 @@
 
 namespace ecs::systems
 {
-    std::function<World(void)> getTestWorld = []() {
-        ecs::World world;
-
-        Entity newEntity = world.registry.spawn_entity();
-
-        world.registry.registerComponent<component::Position>();
-        world.registry.registerComponent<component::Size>();
-        world.registry.registerComponent<component::Drawable>();
-        world.registry.registerComponent<component::Animated>();
-
-        world.registry.addComponent<component::Position>(newEntity, {100, 100});
-        world.registry.addComponent<component::Size>(newEntity, {100, 100});
-        world.registry.addComponent<component::Drawable>(newEntity, {"players", {1, 35, 32, 16}});
-
-        world.addSystem(ecs::systems::draw);
-
-        return world;
-    };
     /**
      * Used to manage Sfml events
      * Currently able to manage the following actions:
@@ -40,36 +22,60 @@ namespace ecs::systems
      * if yes, we had it on world's events' stack, nothing otherwise
      */
     std::function<void(World &)> handleSFMLEvents = [](World &world) {
-        sf::Event event;
-        auto &hitBoxes = world.registry.getComponents<component::Hitbox>();
-        auto &controllables = world.registry.getComponents<component::Controllable>();
+      sf::Event event;
+      auto &hitBoxes = world.registry.getComponents<component::Hitbox>();
+      auto &controllables = world.registry.getComponents<component::Controllable>();
+      auto &activables = world.registry.getComponents<component::Activable>();
+      auto &positions = world.registry.getComponents<component::Position>();
+      auto &sizes = world.registry.getComponents<component::Size>();
+      auto &texts = world.registry.getComponents<component::Text>();
 
-        while (utils::Window::getInstance().pollEvent(event)) {
-            switch (event.type) {
-                case sf::Event::Closed: utils::Window::getInstance().close(); break;
-                case sf::Event::KeyPressed: {
-                    if (event.key.code == sf::Keyboard::Key::W)
-                        ecs::WorldManager::setWaitingWorld(getTestWorld);
-                } break;
-                case sf::Event::KeyReleased: {
-                    for (size_t i = 0; i < controllables.size(); i++) {
-                        auto &contr = controllables[i];
+      while (utils::Window::getInstance().pollEvent(event)) {
+          switch (event.type) {
+              case sf::Event::Closed: utils::Window::getInstance().close(); break;
+              case sf::Event::KeyReleased: {
+                  for (size_t i = 0; i < controllables.size(); i++) {
+                      auto &contr = controllables[i];
 
-                        if (contr == std::nullopt)
-                            continue;
-                        if (event.key.code == contr.value().HitBox) {
-                            for (size_t j = 0; j < hitBoxes.size(); j++) {
-                                auto &hitBox = hitBoxes[j];
+                      if (contr == std::nullopt)
+                          continue;
+                      if (event.key.code == contr.value().HitBox) {
+                          for (size_t j = 0; j < hitBoxes.size(); j++) {
+                              auto &hitBox = hitBoxes[j];
 
-                                if (hitBox)
-                                    hitBox->switchHitBox();
-                            }
-                            break;
-                        }
-                    }
-                }
-                default: break;
-            }
-        }
+                              if (hitBox)
+                                  hitBox->switchHitBox();
+                          }
+                          break;
+                      }
+                  }
+              }
+              case sf::Event::MouseButtonReleased: {
+                  for (size_t i = 0; i < positions.size() && i < activables.size() && i < sizes.size(); i++) {
+                      auto &pos = positions[i];
+                      auto &activ = activables[i];
+                      auto &size = sizes[i];
+                      auto &text = texts[i];
+                      auto mousePosition = sf::Mouse::getPosition(utils::Window::getInstance());
+
+                      if (pos && size && activ) {
+                          if ((mousePosition.x >= pos->x && mousePosition.x <= pos->x + size->width)
+                              && (mousePosition.y >= pos->y && mousePosition.y <= pos->y + size->height)
+                              && activ->getIsButton()) {
+                              if (activ->getButtonType() == utils::constant::PLAY_HOVER)
+                                  ecs::WorldManager::setWaitingWorld([] { return getGameWorld("8000", "localhost"); });
+//                                  if (event.key.code == sf::Mouse::Left)
+//                                      for (size_t j = 0; j < activables.size() && texts.size(); j++)
+//                                          if (activables[j]->getButtonType() == utils::constant::ROOM
+//                                              || activables[j]->getButtonType() == utils::constant::ROOM_TEXT) {
+//                                              activables[j]->switchSetIsActivate();
+//                                          }
+                          }
+                      }
+                  }
+              }
+              default: break;
+          }
+      }
     };
 } // namespace ecs::systems
