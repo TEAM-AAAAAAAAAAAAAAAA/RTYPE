89f84eb885b4bf3c3904fc7eb97565210ac0f017
hourcadettej <julien.hourcadette@live.fr>
2022-11-13 00:02:11 +0100

    feat: add connection component

---
+++ src/ecs/components/client/Connection.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, by hourcadettej on 11/12/22.
+** rtype
+** File description:
+** rtype
+*/
+
+#pragma once
+#include "../../../../src/client/NetworkClient.hpp"
+
+namespace ecs::component {
+    struct Connection {
+
+            Connection(int roomNumber, int port = 8000, const std::string &host = "localhost") : _port(std::to_string(port)), _host(host), _isSet(false) {}
+
+            void setPort(int port) {
+                _port = std::to_string(port);
+                _isSet = true;
+            }
+            inline std::string getPort() const {return _port;}
+            inline void setHost(const std::string &host) {_host = host;}
+            inline std::string getHost() const {return _host;}
+            inline bool getIsSet() const {return _isSet;}
+            inline void setIsSet(bool isSet) {_isSet = isSet;}
+            void setClientConnection() {
+                network::Message msg;
+                msg.fill(0);
+                network::Client::setHost(_host);
+                network::Client::setPort(_port);
+                network::Client::connect();
+                network::Client::getOutgoingMessages().push(msg);
+            }
+        private:
+            std::string _port;
+            std::string _host;
+            bool _isSet;
+    };
+}
