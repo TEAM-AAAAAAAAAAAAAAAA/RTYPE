ced2888eaeb1a929b17592eafd71de7ad8aafd7d
hourcadettej <julien.hourcadette@live.fr>
2022-11-12 17:07:41 +0100

    feat: clear switch case of MenuSelect.hpp

--- src/ecs/systems/client/MenuSelect.hpp
+++ src/ecs/systems/client/MenuSelect.hpp
@@ -34,27 +34,36 @@ namespace ecs::systems
               if ((mousePosition.x >= pos->x && mousePosition.x <= pos->x + size->width) && (mousePosition.y >= pos->y && mousePosition.y <= pos->y + size->height) && activ->getIsButton()) {
                   if (!activ->getIsHover())
                       audio::AudioManager::playSFX("button_hover");
-                  if (activ->getButtonType() != utils::constant::ROOM_HOVER)
-                      draw->rect.top = itHoveredButton->second.rectTop;
                   switch(activ->getButtonType()) {
                       case utils::constant::PLAY: {
                           activ->setButtonType(utils::constant::PLAY_HOVER);
+                          draw->rect.top = itHoveredButton->second.rectTop;
                           draw->rect.left = 220;
+                          draw->rect.width = itHoveredButton->second.defaultRectWidth;
+                          draw->rect.height = itHoveredButton->second.defaultRectHeight;
                           break;
                       }
                       case utils::constant::OPTION: {
                           activ->setButtonType(utils::constant::OPTION_HOVER);
+                          draw->rect.top = itHoveredButton->second.rectTop;
                           draw->rect.left = 3542;
+                          draw->rect.width = itHoveredButton->second.defaultRectWidth;
+                          draw->rect.height = itHoveredButton->second.defaultRectHeight;
                           break;
                       }
                       case utils::constant::QUIT: {
                           activ->setButtonType(utils::constant::QUIT_HOVER);
+                          draw->rect.top = itHoveredButton->second.rectTop;
                           draw->rect.left = 4670;
+                          draw->rect.width = itHoveredButton->second.defaultRectWidth;
+                          draw->rect.height = itHoveredButton->second.defaultRectHeight;
                           break;
                       }
                       case utils::constant::ROOM: {
                           activ->setButtonType(utils::constant::ROOM_HOVER);
                           draw->rect.top = 1510;
+                          draw->rect.width = itHoveredButton->second.defaultRectWidth;
+                          draw->rect.height = itHoveredButton->second.defaultRectHeight;
                           break;
                       }
                       default: break;
@@ -62,41 +71,43 @@ namespace ecs::systems
                   if (activ->getButtonType() == utils::constant::QUIT_HOVER)
                       if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
                           utils::Window::getInstance().close();
-                  draw->rect.width = itHoveredButton->second.defaultRectWidth;
-                  draw->rect.height = itHoveredButton->second.defaultRectHeight;
                   activ->setIsHover(true);
               } else if (activ->getIsHover() && activ->getIsButton()){
-                  activ->getButtonType() == utils::constant::ROOM ? draw->rect.top = itButtonRoom->second.rectTop : draw->rect.top = itButton->second.rectTop;
+                  activ->getButtonType() == utils::constant::ROOM ? draw->rect.top = itButtonRoom->second.rectTop : 0;
                   switch(activ->getButtonType()) {
                       case utils::constant::PLAY_HOVER: {
                           activ->setButtonType(utils::constant::PLAY);
                           draw->rect.left = 324;
+                          draw->rect.top = itButton->second.rectTop;
+                          draw->rect.width = itButton->second.defaultRectWidth;
+                          draw->rect.height = itButton->second.defaultRectHeight;
                           break;
                       }
                       case utils::constant::OPTION_HOVER: {
                           activ->setButtonType(utils::constant::OPTION);
                           draw->rect.left = 3651;
+                          draw->rect.top = itButton->second.rectTop;
+                          draw->rect.width = itButton->second.defaultRectWidth;
+                          draw->rect.height = itButton->second.defaultRectHeight;
                           break;
                       }
                       case utils::constant::QUIT_HOVER: {
                           activ->setButtonType(utils::constant::QUIT);
                           draw->rect.left = 4760;
+                          draw->rect.top = itButton->second.rectTop;
+                          draw->rect.width = itButton->second.defaultRectWidth;
+                          draw->rect.height = itButton->second.defaultRectHeight;
                           break;
                       }
                       case utils::constant::ROOM_HOVER: {
                           activ->setButtonType(utils::constant::ROOM);
                           draw->rect.top = 456;
+                          draw->rect.width = itButtonRoom->second.defaultRectWidth;
+                          draw->rect.height = itButtonRoom->second.defaultRectHeight;
                           break;
                           default:
                               break;
                       }
-                  }
-                  if (activ->getButtonType() == utils::constant::ROOM) {
-                      draw->rect.width = itButtonRoom->second.defaultRectWidth;
-                      draw->rect.height = itButtonRoom->second.defaultRectHeight;
-                  } else {
-                      draw->rect.width = itButton->second.defaultRectWidth;
-                      draw->rect.height = itButton->second.defaultRectHeight;
                   }
                   activ->setIsHover(false);
               }
