0c174e89d6db7c1697fd15dc7813bf751d1435de
Guillaume <guillaume.terriere@epitech.eu>
2022-11-13 16:14:26 +0100

    fix: add protection to getContent of text

--- src/ecs/systems/client/ScoreUpdate.hpp
+++ src/ecs/systems/client/ScoreUpdate.hpp
@@ -7,12 +7,12 @@
 
 #pragma once
 
+#include <chrono>
 #include <functional>
 #include <iostream>
 #include "World.hpp"
-#include "components/client/Text.hpp"
 #include "components/Score.hpp"
-#include <chrono>
+#include "components/client/Text.hpp"
 
 static std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
@@ -22,21 +22,23 @@ namespace ecs::systems
      * Used to set the score of the player
      */
     std::function<void(World &)> scoreUpdate = [](World &world) {
-      auto &texts = world.registry.getComponents<component::Text>();
-      auto &scores = world.registry.getComponents<component::Score>();
+        auto &texts = world.registry.getComponents<component::Text>();
+        auto &scores = world.registry.getComponents<component::Score>();
 
-      for (size_t i = 0; i < texts.size() || i < scores.size(); i++) {
-          auto &text = texts[i];
-          auto &score = scores[i];
+        for (size_t i = 0; i < texts.size() || i < scores.size(); i++) {
+            auto &text = texts[i];
+            auto &score = scores[i];
 
-          if (text && score && text.value().getContent(0) == "Score: ") {
-              std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-              auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 10;
-              auto elapsed = std::to_string(time);
+            if (text && score) {
+                if (text.value().getContent(0) == "Score: ") {
+                    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+                    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 10;
+                    auto elapsed = std::to_string(time);
 
-              text->setContent(1, elapsed);
-              score->setScore(time);
-          }
-      }
+                    text->setContent(1, elapsed);
+                    score->setScore(time);
+                }
+            }
+        }
     };
 } // namespace ecs::systems
